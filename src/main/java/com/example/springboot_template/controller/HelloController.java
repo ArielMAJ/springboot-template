a213b6b3c6cc32924c9835cf6b7e5cde1d5a961a
ArielMAJ <ariel.maj@hotmail.com>
2024-12-01 18:47:38 -0300

    [ADD] Add base java-springboot backend template files

---
+++ src/main/java/com/example/springboot_template/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.springboot_template.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("<h1>Hello %s!</h1>", name);
+    }
+}
+
+
+
